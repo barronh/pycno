f0c88240429680f44904efd2d3f6fc7b5f0facaa
barronh <barronh@gmail.com>
2020-08-15 07:25:05 -0400

    central data location
    adding data location via environment, home or .

--- pycno/__init__.py
+++ pycno/__init__.py
@@ -1,11 +1,14 @@
 __all__ = ['cno']
 
 import os
+from pathlib import Path
+import warnings
 import numpy as np
 
 class cno:
     def __init__(
-        self, proj=None, xlim=(None, None), ylim=(None, None), clipax=True
+        self, proj=None, xlim=(None, None), ylim=(None, None), clipax=True,
+        data=None
     ):
         """
         CNO and CNOB overlay reader and plotter.
@@ -18,8 +21,12 @@ class cno:
             length 2 tuple where values must be accepted by set_xlim
         ylim : tuple
             length 2 tuple where values must be accepted by set_ylim
-        clipax: bool
-            If True, use ax.set_xlim and ax.set_ylim with xlim and ylim parameters
+        clipax : bool
+            If True, use ax.set_xlim and ax.set_ylim with xlim and ylim 
+            parameters
+        data : str
+            Optional, path to downloaded cno files. Defaults to environmental
+            variable PYCNO_DATA, if not defined, defaults to .
         """
         if isinstance(proj, str):
             import pyproj
@@ -29,14 +36,29 @@ class cno:
         self._proj = proj
         self._clipax = clipax
         self._cachedfeatures = {}
-    
-    def getfeatures(self, cnobpath):
+        if data is None:
+            data = os.environ.get('PYCNO_DATA', None)
+            print(data)
+
+        if data is None:
+            data = Path.home() / '.pycno'
+
+        if not os.path.exists(data):
+            warnings.warn('Path does not exist: ' + str(data) + '; default .')
+            warnings.filterwarnings(
+                "ignore", category=UserWarning, module=__name__, lineno=47
+            )
+            data = '.'
+
+        self._data = Path(data)
+
+    def getfeatures(self, cnopath):
         """
         Get coordinates for each feature.
         
         Arguments
         ---------
-        cnobpath : str
+        cnopath : str
           path to cnob file
         
         Returns
@@ -44,28 +66,27 @@ class cno:
         features : list
           list of features where each feature is (x, y)
         """
-        if cnobpath not in self._cachedfeatures:
-            if not os.path.exists(cnobpath):
-                cno._getoverlay(cnobpath)
-            self._cachedfeatures[cnobpath] = self._parseoverlay(cnobpath)
-        return self._cachedfeatures[cnobpath]
+        if cnopath not in self._cachedfeatures:
+            cnorealpath = self._getoverlay(cnopath)
+            self._cachedfeatures[cnopath] = self._parseoverlay(cnorealpath)
+        return self._cachedfeatures[cnopath]
 
-    def draw(self, cnobpath='MWDB_Coasts_Countries_3.cnob', ax=None):
+    def draw(self, cnopath='MWDB_Coasts_Countries_3.cnob', ax=None):
         if ax is None:
             import matplotlib.pyplot as plt
             ax = plt.gca()
-        features = self.getfeatures(cnobpath)
+        features = self.getfeatures(cnopath)
         for flon, flat in features:
             ax.plot(flon, flat, color='k', linewidth=0.75, alpha=0.7)
         if self._clipax:
             ax.set_xlim(*self._xlim)
             ax.set_ylim(*self._ylim)
 
-    def _parseoverlay(self, cnobpath):
+    def _parseoverlay(self, cnopath):
         """
         Arguments
         ---------
-        cnobpath : str
+        cnopath : str
           path to CNOB path
         
         Returns
@@ -73,7 +94,7 @@ class cno:
         features : list
           list of (lons, lats) tuples
         """
-        f = open(cnobpath, 'rb')
+        f = open(cnopath, 'rb')
         buff = np.frombuffer(f.read(), '>i')
         check = buff[:2].view('S8')[0] == b'GISSCNOB'
         if not check:
@@ -106,11 +127,18 @@ class cno:
             if np.ma.getmaskarray(y).all():
                 continue
             features.append((x, y))
-            
+
         return features
 
-    @classmethod
-    def _getoverlay(cls, cnobpath):
+    def _getoverlay(self, cnopath):
         from urllib.request import urlretrieve
-        urlretrieve('https://www.giss.nasa.gov/tools/panoply/overlays/' + cnobpath, cnobpath)
-    
+        cnopatho = Path(cnopath)
+        if cnopatho.exists():
+            return cnopatho
+        cnopatho = self._data / cnopatho
+        if not cnopatho.exists():
+            panoplyurl = 'https://www.giss.nasa.gov/tools/panoply/overlays/'
+            url = panoplyurl + cnopath
+            warnings.warn('Downloading: ' + url + ' to ' + str(cnopatho))
+            urlretrieve(url, cnopatho)
+        return cnopatho
