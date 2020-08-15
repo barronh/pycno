ddb22ad173982a62c081ef1f0e37ff808d2fedfd
barronh <barronh@gmail.com>
2020-08-15 07:34:53 -0400

    typo
    nco should be cno... I love nco, but this is not it.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pynco",
+    name="pycno",
     version="0.0.1",
     author="Barron H. Henderson",
     author_email="barronh@gmail.com",
