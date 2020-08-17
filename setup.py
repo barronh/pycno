00de9b3e82851b24fb62320a501b6c9cbd19e9e3
barronh <barronh@gmail.com>
2020-08-17 11:17:50 -0400

    increment version
    mainly for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycno",
-    version="0.0.1",
+    version="0.0.2",
     author="Barron H. Henderson",
     author_email="barronh@gmail.com",
     description="Python map overlay software to read CNO and CNOB files.",
